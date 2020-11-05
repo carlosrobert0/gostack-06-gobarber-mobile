0d00b437ea11d2bd879caabeda17093092a96753
Carlos Roberto <carlosroberto15755@gmail.com>
2020-11-05 00:20:49 -0300

    Autenticacao de usuarios

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -15,6 +15,8 @@ import * as Yup from 'yup'
 import { Form } from '@unform/mobile'
 import { FormHandles } from '@unform/core'
 
+import { useAuth } from '../../hooks/auth'
+
 import getValidationErrors from '../../utils/getValidationErrors'
 
 import Input from '../../components/Input'
@@ -39,7 +41,12 @@ interface SignInFormData {
 const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null)
   const passwordInputRef = useRef<TextInput>(null)
+
   const navigation = useNavigation()
+
+  const { signIn, user } = useAuth()
+
+  console.log(user)
 
   const handleSignIn = useCallback(async (data: SignInFormData) => {
     try {
@@ -54,12 +61,10 @@ const SignIn: React.FC = () => {
         abortEarly: false
       })
 
-      // await signIn({
-      //   email: data.email,
-      //   password: data.password
-      // })
-
-      // history.push('/dashboard')
+      await signIn({
+        email: data.email,
+        password: data.password
+      })
     } catch (err) {
       if (err instanceof Yup.ValidationError) {
         const errors = getValidationErrors(err)
@@ -74,7 +79,7 @@ const SignIn: React.FC = () => {
         'Ocorreu um erro ao fazer login, cheque as credenciais',
       )
     }
-  }, [])
+  }, [signIn])
 
   return (
     <>
